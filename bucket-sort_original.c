983fcdffbb5f16d21e7badfc59f506b8ccd7775b
eduardoeiras <61152075+eduardoeiras@users.noreply.github.com>
2021-12-16 11:33:33 +0000

    algoritmo original

--- bucket-sort_original.c
+++ bucket-sort_original.c
@@ -71,6 +71,70 @@ void BucketSort(int arr[]) {
   return;
 }
 
+struct Node *QuickSort(struct Node *list)
+{
+   struct Node *k, *nodeList;
+   if (list == 0 || list->next == 0) {
+    return list;
+  }
+  nodeList = list;
+  k = list->next;
+  nodeList->next = 0;
+
+};
+
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+
+int partition(int array[], int low, int high) {
+  
+  // select the rightmost element as pivot
+  int pivot = array[high];
+  
+  // pointer for greater element
+  int i = (low - 1);
+
+  // traverse each element of the array
+  // compare them with the pivot
+  for (int j = low; j < high; j++) {
+    if (array[j] <= pivot) {
+        
+      // if element smaller than pivot is found
+      // swap it with the greater element pointed by i
+      i++;
+      
+      // swap element at i with element at j
+      swap(&array[i], &array[j]);
+    }
+  }
+
+  // swap the pivot element with the greater element at i
+  swap(&array[i + 1], &array[high]);
+  
+  // return the partition point
+  return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+  if (low < high) {
+    
+    // find the pivot element such that
+    // elements smaller than pivot are on left of pivot
+    // elements greater than pivot are on right of pivot
+    int pi = partition(array, low, high);
+    
+    // recursive call on the left of pivot
+    quickSort(array, low, pi - 1);
+    
+    // recursive call on the right of pivot
+    quickSort(array, pi + 1, high);
+  }
+}
+
+
 // Function to sort the elements of each bucket
 struct Node *InsertionSort(struct Node *list) {
   struct Node *k, *nodeList;
